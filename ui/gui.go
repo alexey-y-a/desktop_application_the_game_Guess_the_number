603ca3103e3575db43812454fbc1bcf5d2e6ec7f
alexey-y-a <lexus2157@mail.ru>
2025-06-21 01:45:15 +0300

    step_04 done

--- ui/gui.go
+++ ui/gui.go
@@ -1,1 +1,66 @@
 package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"game/internal/game"
+	"strconv"
+)
+
+func Run() {
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Угадай число!")
+
+	g := &game.Game{GuessesLeft: -1}
+
+	computerGuessLabel := widget.NewLabel("Нажмите кнопку ниже, чтобы компьютер загадал число между 0 и 300!")
+
+	startGameButton := widget.NewButton("Загадать новое число!", func() {
+		g.StartNewGame()
+		computerGuessLabel.SetText("Компьютер загадал число! Попыток осталось: " + strconv.Itoa(g.GuessesLeft))
+	})
+
+	guessDisplay := widget.NewLabel("Введите ваше первое число!")
+
+	userGuessInput := widget.NewEntry()
+	userGuessInput.SetPlaceHolder("Введите ваше число: ")
+
+	tryGuessButton := widget.NewButton("Попробовать", func() {
+		userGuess, err := strconv.Atoi(userGuessInput.Text)
+		if err != nil {
+			guessDisplay.SetText("Пожалуйста, введите число!")
+			return
+		}
+
+		guessResult, statusText, gameEnded := g.TryGuess(userGuess)
+		guessDisplay.SetText(guessResult)
+		if statusText != "" {
+			computerGuessLabel.SetText(statusText)
+		}
+		if gameEnded {
+			g.GuessesLeft = -1
+		}
+		userGuessInput.SetText("")
+	})
+
+	userGuessInput.OnSubmitted = func(text string) {
+		tryGuessButton.OnTapped()
+	}
+
+	myWindow.SetContent(
+		container.NewCenter(
+			container.NewVBox(
+				computerGuessLabel,
+				startGameButton,
+				guessDisplay,
+				userGuessInput,
+				tryGuessButton,
+			),
+		),
+	)
+
+	myWindow.Resize(fyne.NewSize(300, 200))
+	myWindow.ShowAndRun()
+}
